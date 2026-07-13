9a56064e3603f705cdf5db0ab7b06ef01befc47b
Tom <vlqvinh444@gmail.com>
2026-07-13 09:00:55 +0700

    First Commit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,31 @@
+import { navLinks } from "../constants";
+
+const NavBar = () => {
+  return (
+    <header>
+      <nav>
+        <img src="/logo.svg" alt="Apple Logo" />
+
+        <ul>
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a href={item.label.toLowerCase()}>{item.label}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="flex items-center gap-3">
+          <button>
+            <img src="/search.svg" alt="Search" />
+          </button>
+
+          <button>
+            <img src="/cart.svg" alt="Cart" />
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
---
+++ src/constants/index.ts
@@ -0,0 +1,23 @@
+const navLinks = [
+  { label: "Store" },
+  {
+    label: "Mac",
+  },
+  {
+    label: "iPhone",
+  },
+  {
+    label: "Watch",
+  },
+  {
+    label: "Vision",
+  },
+  {
+    label: "AirPods",
+  },
+];
+
+
+export {
+  navLinks
+}
